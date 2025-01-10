c14477b1f0472982d4e50239a7c367fcd5de413c
muhurto-rahman <muhurtorahman@gmail.com>
2025-01-10 16:32:54 -0500

    Attempt 4 at making selenium work for me

--- PerfectPick.py
+++ PerfectPick.py
@@ -26,9 +26,8 @@ def get_driver():
     return webdriver.Chrome(service=Service(ChromeDriverManager().install()), options=options)
 #Fix selenium issues 
 options = Options()
-options.add_argument("--headless")
-options.add_argument('--disable-gpu')
-options.add_argument("--no-sandbox")
+options.add_argument("--window-size=1920,1080")
+options.add_argument('--headless')
 
 
 
